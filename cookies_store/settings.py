36e2cc60ceb344791564a96d2fd1bab2f4edf8c2
sarahsaphira <sarahsaphira80@gmail.com>
2024-11-20 08:59:10 +0700

    Tugas 9

--- cookies_store/settings.py
+++ cookies_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-r-1_t#fa17z%27+2jcd5kfxs!%q$8j=4l-1pq(34uwqmuvl!fj
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sarah-saphira31-cookiesstore.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sarah-saphira31-cookiesstore.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 # Application definition
 
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'cookies_store.urls'
@@ -131,3 +133,9 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ from main.views import logout_user
 from main.views import edit_product
 from main.views import delete_product
 from main.views import add_product_ajax
+from main.views import create_product_flutter
 
 app_name = 'main'
 
@@ -22,4 +23,5 @@ urlpatterns = [
     path('edit-product/<uuid:id>', edit_product, name='edit_product'),
     path('delete/<uuid:id>', delete_product, name='delete_product'),
     path('create-product-ajax', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
